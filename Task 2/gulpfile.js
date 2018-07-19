3352eee9a004577a018e08711e9d650996f40467
a.magomedov <arkadiy.magomedov@stoloto.ru>
2018-07-19 08:36:48 +0300

    added gulp config for babel

--- Task 2/gulpfile.js
+++ Task 2/gulpfile.js
@@ -2,9 +2,12 @@ const gulp = require("gulp");
 const sass = require("gulp-sass");
 const sourcemaps = require("gulp-sourcemaps");
 const autoprefixer = require("gulp-autoprefixer");
+const babel = require("gulp-babel");
 
-const input = "./src/scss/**/*.scss";
-const output = "./build/css";
+const inputJS = "./src/js/app.js";
+const inputSCSS = "./src/scss/**/*.scss";
+const outputCSS = "./build/css";
+const outputJS = "./build/js";
 
 const sassOptions = {
   errLogToConsole: true,
@@ -15,22 +18,38 @@ const autoprefixerOptions = {
   browsers: ["last 2 versions", "> 5%", "Firefox ESR"]
 };
 
+gulp.task("babel", () =>
+  gulp
+    .src(inputJS)
+    .pipe(
+      babel({
+        presets: ["@babel/env"]
+      })
+    )
+    .pipe(gulp.dest(outputJS))
+);
+
 gulp.task("sass", function() {
   return gulp
-    .src(input)
+    .src(inputSCSS)
     .pipe(sourcemaps.init())
     .pipe(sass(sassOptions).on("error", sass.logError))
     .pipe(autoprefixer(autoprefixerOptions))
     .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest(output));
+    .pipe(gulp.dest(outputCSS));
 });
+
+// gulp.task("watch", function() {
+//   return gulp.watch(input, ["sass"]).on("change", function(event) {
+//     console.log(
+//       "File " + event.path + " was " + event.type + ", running tasks..."
+//     );
+//   });
+// });
 
 gulp.task("watch", function() {
-  return gulp.watch(input, ["sass"]).on("change", function(event) {
-    console.log(
-      "File " + event.path + " was " + event.type + ", running tasks..."
-    );
-  });
+  gulp.watch(inputSCSS, ["sass"]);
+  gulp.watch(inputJS, ["babel"]);
 });
 
-gulp.task("default", ["sass", "watch" /*, possible other tasks... */]);
+gulp.task("default", ["sass", "babel", "watch"]);
