3d0a3ebae68cd026985ac58d7bc1f942bc3e4c42
a.magomedov <arkadiy.magomedov@stoloto.ru>
2018-07-26 10:30:34 +0300

    added modal animation

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
 import "babel-polyfill";
+import "classlist-polyfill";
 
 import { Dial } from "./Dial";
 import { Scroll } from "./Scroll";
@@ -20,26 +21,80 @@ const target = document.getElementById("scenarios-pages");
 new Scroll(leftBtn, rightBtn, target);
 
 const modal = document.querySelector(".modal");
+const modalControls = document.querySelector(".modal__controls");
 const closeModalButton = document.getElementById("closeModal");
 
 closeModalButton.addEventListener("click", () => {
   closeModal();
 });
 
+/**
+ * Opening modal with animation
+ */
 document.addEventListener(
   "click",
   e => {
     if (e.target.classList.contains("action-box")) {
-      openModal();
+      openModal(e.target);
     }
   },
   true
 );
 
-function openModal() {
-  modal.style.visibility = "visible";
+/**
+ * TODO: NEED TO POPULATE THE MODAL BEFORE ANIMATION
+ */
+
+function openModal(eventSource) {
+  // Get event sources dimensions
+  const sourceValues = eventSource.getBoundingClientRect();
+  const modalWrapper = document.querySelector(".modal__wrapper");
+
+  // Get the modal's init values;
+  const { x, y, width, height } = modalWrapper.getBoundingClientRect();
+
+  // Change modal's position to absolute and resize to initial values
+  modalWrapper.style.position = "absolute";
+  restore();
+
+  // Request next animation frame and position on top of the event source
+  requestAnimationFrame(reposition);
+
+  function reposition() {
+    modalWrapper.style.top = `${sourceValues.y}px`;
+    modalWrapper.style.left = `${sourceValues.x}px`;
+
+    // Scale to the source proportions
+    modalWrapper.style.transform = `scaleX(${sourceValues.width /
+      width}) scaleY(${sourceValues.height / height})`;
+
+    modal.classList.add("modal_visible");
+
+    // Add animation class
+    requestAnimationFrame(() => {
+      modalWrapper.classList.add("ready");
+      // Ready to animate
+      requestAnimationFrame(restore);
+    });
+  }
+
+  function restore() {
+    modalWrapper.addEventListener("transitionend", restorePosition);
+    modalWrapper.style.top = `${y}px`;
+    modalWrapper.style.left = `${x}px`;
+    modalWrapper.style.width = `${width}px`;
+    modalWrapper.style.height = `${height}px`;
+    modalWrapper.style.transform = `scaleX(1) scaleY(1)`;
+  }
+  function restorePosition() {
+    modalWrapper.removeEventListener("transitionend", restorePosition);
+    modalWrapper.style = "";
+    modalWrapper.classList.remove("ready");
+    modalControls.classList.add("modal__controls_visible");
+  }
 }
 
 function closeModal() {
-  modal.style.visibility = "hidden";
+  modal.classList.remove("modal_visible");
+  modalControls.classList.remove("modal__controls_visible");
 }
